f9aadfbe93e0b735666e5c4f34292367b88b3521
Unduck <74640695+csc7545@users.noreply.github.com>
2026-03-12 20:53:58 +0900

    [Bronze III] Title: 통학의 신, Time: 192 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,18 +1,21 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        int x1 = (int)(-a - Math.sqrt(a*a - b));
-        int x2 = (int)(-a + Math.sqrt(a*a - b));
-        
-        if(x1 == x2){
-            System.out.println(x1);
-        } else {
-            System.out.println(x1 + " " + x2);
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    
+    int x1 = 0;
+    int x2 = 0;
+    
+    x1 = (int)(-A - Math.sqrt(A*A - B));
+    x2 = (int)(-A + Math.sqrt(A*A - B));
+    
+    if(x1 == x2){
+        System.out.println(x1);
+    } else{
+        System.out.println(x1 + " " + x2);
     }
+  }
 }
